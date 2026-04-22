f4ef9b21757c574679d73c41bed900ecb35df0f4
Macjim202 <macjim202@gmail.com>
2026-04-22 21:32:14 +0500

    Task5

---
+++ Tyuiu.ZaitsevMS.Sprint4.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ZaitsevMS.Sprint4.Task5.V0.Lib;
+
+namespace Tyuiu.ZaitsevMS.Sprint4.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[3, 3] { { 0, 0, 1 }, { 1, 0, 1 }, { 1, 1, 0 } };
+            int res = ds.Calculate(mas2);
+            int wait = 4;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
--- Tyuiu.ZaitsevMS.Sprint4.Task5.V0/Program.cs
+++ Tyuiu.ZaitsevMS.Sprint4.Task5.V0/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tyuiu.ZaitsevMS.Sprint4.Task5.V0.Lib;
 
 namespace Tyuiu.ZaitsevMS.Sprint4.Task5.V0
 {
@@ -10,6 +11,57 @@ namespace Tyuiu.ZaitsevMS.Sprint4.Task5.V0
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+
+            Console.Title = "Спринт #4 | Выполнил: Зайцев М. С. | ИБКСб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные масивы (статический ввод)                               *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Зайцев Максим Сергеевич | ИБКСб-25-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите кол-во строк: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите кол-во столбцов: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mtrx = new int[rows, columns];
+
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    mtrx[i, j] = rnd.Next(-1, 1);
+                }
+            }
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0;i < rows; i++)
+            {
+                for (int j = 0;j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Кол-во нулей = " + res);
+            Console.ReadLine();
         }
     }
 }
